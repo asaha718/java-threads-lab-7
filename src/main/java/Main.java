6a19b785cf83486d986fecccf61e4994a8293ce6
Anug Saha <anugsaha@gmail.com>
2022-07-27 13:32:11 -0400

    return callable ...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,19 @@ import java.util.Scanner;
 import java.util.concurrent.Callable;
 
 public class Main {
+    public static void main(String[] args) throws Exception{
+
+        System.out.println("Returns Callable " + getCallableInteger().call());
+    }
     public static Callable<Integer> getCallableInteger() {
         // your code here
+        Callable<Integer> intCallable = () -> {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter number: ");
+            Integer num = scanner.nextInt();
+            return num;
+        };
+
+        return intCallable;
     }
 }
